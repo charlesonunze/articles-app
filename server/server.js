1e56462b30f93bd5c5ba5fec6383020d424d46c1
charlesonunze <charlesonunze2@gmail.com>
2018-04-14 16:15:56 +0100

    added model for users and refactored routing

--- server/server.js
+++ server/server.js
@@ -1,16 +1,13 @@
-const _ = require('lodash');
 const hbs = require('hbs');
 const path = require('path');
-const bcrypt = require('bcryptjs');
 const express = require('express');
 const session = require('express-session');
 const flash = require('connect-flash');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const passport = require('passport');
-// const messages = require('express-messages');
+const LocalStrategy = require('passport-local');
 
-const {ObjectID} = require('mongodb');
 const {mongoose} = require('./db/mongoose');
 const {Article} = require('./models/article');
 
@@ -51,104 +48,9 @@ app.get('/', (req, res) => {
       res.render('index', {articles});
     });
 });
-
-app.get('/articles/add', (req, res) => {
-  res.render('add-article');
-});
-
-app.post('/articles/add', (req, res) => {
-  let body = _.pick(req.body, ['title', 'author', 'body']);
-
-  const article = new Article({title: body.title, author: body.author, body: body.body});
-
-  article
-    .save()
-    .then((doc) => {
-      req.flash('success_msg', 'new article added');
-      res.redirect('/');
-    })
-    .catch((e) => {
-      res
-        .status(400)
-        .send(e);
-    });
-});
-
-app.get('/article/:id', (req, res) => {
-  Article
-    .findById(req.params.id)
-    .then((article) => {
-      res.render('article', {article});
-    })
-    .catch((e) => {
-      res
-        .status(400)
-        .send(e);
-    });
-});
-
-app.get('/article/edit/:id', (req, res) => {
-  Article
-    .findById(req.params.id)
-    .then((article) => {
-      res.render('edit-article', {article});
-    })
-    .catch((e) => {
-      res
-        .status(400)
-        .send(e);
-    });
-});
-
-app.post('/article/edit/:id', (req, res) => {
-  let id = req.params.id;
-  let body = _.pick(req.body, ['title', 'author', 'body']);
-
-  // validateID(id)
-  if (!ObjectID.isValid(id)) 
-    return res
-      .status(404)
-      .send({Error: 'Invalid ID'});
-  
-  Article
-    .findOneAndUpdate({
-      _id: id
-    }, {
-      $set: body
-    }, {new: true})
-    .then((article) => {
-      if (!article) 
-        return res
-          .status(404)
-          .send({Error: 'Page not found'});
-      
-      res.redirect(`/article/${id}`);
-    })
-    .catch((e) => {
-      res
-        .status(400)
-        .send(e);
-    });
-});
-// delete
-app.post('/article/:id', (req, res) => {
-
-  Article
-    .findByIdAndRemove(req.params.id)
-    .then((article) => {
-      if (!article) 
-        return res
-          .status(404)
-          .send({Error: 'Page not found'});
-      req.flash('success_msg', 'article deleted');
-      res.redirect(301, '/');
-    })
-    .catch((e) => {
-      res
-        .status(400)
-        .send(e);
-    });
-});
+//
+let articlesRoute = require('../routes/articles');
+app.use('/article', articlesRoute);
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
